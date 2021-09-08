c9833cb0e9b28084f8a90c895d8c7460c84d2614
Leon You <32273153+lyou8727@users.noreply.github.com>
2021-09-08 19:14:37 -0400

    Add files via upload
    Recitation first lab

---
+++ recitation1mgs314.py
@@ -0,0 +1,12 @@
+x='H','E','L','L','O'
+print(type(x))
+
+marks_obtained = 25
+total_marks = 37
+percentage = ((marks_obtained/total_marks)*100)
+strpercentage = str(percentage)
+print(strpercentage+'%')
+    
+y = " I am a UB Student"
+print(y[0:10])
+print(y[0:-7])
